2188070ff5e5ff459610832f187268019b06404e
Ahmet Numan  Ecevitci <41120508+ahmetnuman42@users.noreply.github.com>
2022-09-23 17:29:54 +0300

    Add files via upload

---
+++ Karakter_Tersten_Yazdırma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Karakter_Tersten_Yazdırma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var metin=Console.ReadLine().Split(' ');
+
+            foreach(var i in metin)
+            {
+                string ters = i;
+                string basharf = ters[0].ToString();
+                ters = ters.Remove(0, 1);
+                ters = ters+basharf;
+                Console.Write(ters+" ");
+            }
+        }
+    }
+}
