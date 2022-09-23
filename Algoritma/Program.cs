2997ab780fd5973b5d10bd5ce3d9d17941ef32c3
Ahmet Numan  Ecevitci <41120508+ahmetnuman42@users.noreply.github.com>
2022-09-23 17:17:22 +0300

    Add files via upload

---
+++ Algoritma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algoritma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir kelime ve kaçıncı karakterini çıkartmak istiyorsanız indexini yazınız. ===>>> (kelime,index): ");
+                var entry = Console.ReadLine().Split(',');           
+                string name = entry[0];
+                int index = Convert.ToInt32(entry[1]);   
+                name=name.Remove(index-1,1);
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
