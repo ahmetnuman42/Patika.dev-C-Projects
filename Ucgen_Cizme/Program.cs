abb58b496cb73d694fdc4189a4f44760f159d258
Ahmet Numan  Ecevitci <41120508+ahmetnuman42@users.noreply.github.com>
2022-09-23 16:02:06 +0300

    Add files via upload

---
+++ Ucgen_Cizme/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ucgen_Cizme
+{
+    class Program
+    {
+        static void Main()
+        {  
+            Console.WriteLine("Üçgenin boyutunu giriniz.");
+             int boyut = Convert.ToInt32(Console.ReadLine());
+            Cisim.UcgenCiz(boyut);           
+        }
+    }
+    class Cisim
+    {
+        public static void UcgenCiz(int boyut)
+        {
+            for (int i = 1; i < boyut + 1; i++)
+            {
+                for (int j = i; j < boyut + 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
