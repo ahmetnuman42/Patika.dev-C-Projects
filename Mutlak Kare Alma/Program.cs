30f6e1c8cdc6887fa21ba7f825927a6373028427
Ahmet Numan  Ecevitci <41120508+ahmetnuman42@users.noreply.github.com>
2022-09-24 19:55:41 +0300

    Add files via upload

---
+++ Mutlak Kare Alma/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mutlak_Kare_Alma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arada boşluklar bırakarak sayi giriniz :");
+            var dizi = Console.ReadLine().Split(' ');
+            List<int> buyukolanlardizisi = new();
+            List<int> kucukolanlardizisi = new();
+            for (int i = 0; i <dizi.Length; i++)
+            {
+                int a = int.Parse(dizi[i]);
+                if (a<67)
+                {
+                    int temp =67-a;
+                    kucukolanlardizisi.Add(temp);
+                }
+                else if(a>67)
+                {
+                    int o = (a - 67)* (a - 67);                    
+                    buyukolanlardizisi.Add(o);
+                }
+            }
+            int toplamkucuk = 0;
+            foreach (var a in kucukolanlardizisi)
+            {
+                toplamkucuk = toplamkucuk + a;
+                
+            }Console.Write(toplamkucuk + " ");
+            int toplambuyuk = 0;
+            foreach (var a in buyukolanlardizisi)
+            {
+                toplambuyuk = toplambuyuk + a;
+                
+            }Console.Write(toplambuyuk + " ");
+        }
+    }
+}
