cddd4dce7a99e9fd91d24a5e2922d88aefea18db
Ahmet Numan  Ecevitci <41120508+ahmetnuman42@users.noreply.github.com>
2022-09-24 20:19:07 +0300

    Add files via upload

---
+++ Karakter_Değiştirme/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Karakter_Değiştirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var metin = Console.ReadLine().Split(' ');
+
+            foreach (var i in metin)
+            {
+                int boyut = i.Length;
+                string ters = i;
+                string basharf = ters[0].ToString();
+                string sonharf = ters[i.Length - 1].ToString();
+                ters = ters.Remove(0, 1);
+                ters = ters.Remove(boyut-2,1);
+                ters = sonharf+ters + basharf;
+                Console.Write(ters + " ");
+            }
+
+        }
+    }
+}
