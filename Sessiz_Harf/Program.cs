0b1296d2ef7f8ea0d64adb4853457cca5f0000b4
Ahmet Numan  Ecevitci <41120508+ahmetnuman42@users.noreply.github.com>
2022-09-25 13:10:10 +0300

    Add files via upload

---
+++ Sessiz_Harf/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sessiz_Harf
+{
+    class Program
+    {
+        static void Main()
+        {
+            var metin = Console.ReadLine().Split(' ');
+            string sesliharfler = "aeıioöuü";
+            int trusayac = 0;
+            List<bool> sonuc = new();
+            //int falssayac = 0;
+            for(int i = 0; i < metin.Length; i++)
+            {
+                string kelime = metin[i].Trim();
+                for(int j = 0; j <kelime.Length; j++) 
+                {                     
+                      if(sesliharfler.Contains(kelime[j]))
+                        {
+                            trusayac = 0;
+                        }
+                        else
+                        {
+                            trusayac++;
+                            if (trusayac == 2)
+                            {                               
+                                sonuc.Add(true);
+                                break;
+                            }
+                        }                    
+                }                    
+            }
+            foreach (var item in sonuc)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
